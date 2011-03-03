6a711900aa63290a7deff0d34cfabbaf02696070
Wolfger Schramm <wolfger@spearwolf.de>
2011-03-03 15:15:06 +0100

    writeHeader

--- server.js
+++ server.js
@@ -11,19 +11,19 @@ var server = http.createServer(function(request, response) {
     var filename = path.join(process.cwd(), uri);  
     path.exists(filename, function(exists) {  
         if (!exists) {  
-            response.sendHeader(404, {"Content-Type": "text/plain"});  
+            response.writeHeader(404, {"Content-Type": "text/plain"});  
             response.write("404 Not Found\n");  
             response.close();  
             return;  
         }  
         fs.readFile(filename, "binary", function(err, file) {  
             if (err) {  
-                response.sendHeader(500, {"Content-Type": "text/plain"});  
+                response.writeHeader(500, {"Content-Type": "text/plain"});  
                 response.write(err + "\n");  
                 response.close();  
                 return;  
             }  
-            response.sendHeader(200);  
+            response.writeHeader(200);  
             response.write(file, "binary");  
             response.close();  
         });  
